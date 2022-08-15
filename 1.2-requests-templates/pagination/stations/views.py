cd0a5cb61b5eb59b1db34dee852cd3f4f8e1e4fe
katerina-arkhipenkova <katerina.arkhipenkova@yandex.ru>
2022-08-15 23:38:04 +0300

    pagination commit

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+import csv
+
+from pagination import settings
 
 
 def index(request):
@@ -7,11 +11,18 @@ def index(request):
 
 
 def bus_stations(request):
+    page_number = int(request.GET.get("page", 1))
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
+    with open(settings.BUS_STATION_CSV, encoding='utf-8') as f:
+        rows = csv.DictReader(f)
+        station_list = list(rows)
+
+    paginator = Paginator(station_list, 10)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        # 'bus_stations': station_list,
+        'page': page
     }
     return render(request, 'stations/index.html', context)
